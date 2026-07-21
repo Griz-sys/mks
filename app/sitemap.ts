a4d0f6f4e5c33121a2615d224d40d14e50d5bd6f
shahnawaz-soft <shahnawaz@vecube.club>
2026-07-21 18:02:51 +0530

    Updated SEO part

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const base = 'https://www.mkstandoori.in'
+  const base = 'https://www.mkstandoori.com'
   const now = new Date()
 
   return [
